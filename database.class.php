ab4d7f8640f7cfffa41703aae651345861e875f3
JobScholt3n <60648149+JobScholt3n@users.noreply.github.com>
2021-05-20 14:22:26 +0200

    Update database.class.php

--- database.class.php
+++ database.class.php
@@ -1,22 +1,21 @@
 <?php
 class Database
 {
-    private $servername = 'jscholten@gc-webhosting.nl';
-    private $username = "jscholten_dbuser";
-    private $password = "prH1Ku#xjqf3";
-    private $dbname = "jscholten_netland";
+    private $servername = 'localhost';
+    private $username = "root";
+    private $password = "";
+    private $dbname = "netland";
     private $charset = "utf8mb4";
 
     public function connect()
     {
-        try{
-            
-        $dsn = "mysql:host=" . $this->servername . ";dbname=" . $this->dbname . ";charset=" . $this->charset;
-        $pdo = new PDO($dsn, $this->username, $this->password);
-        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        return $pdo;
-        }catch(PDOexception $e){
-        echo "Connection failed: " . $e->getMessage();
+        try {
+            $dsn = "mysql:host=" . $this->servername . ";dbname=" . $this->dbname . ";charset=" . $this->charset;
+            $pdo = new PDO($dsn, $this->username, $this->password);
+            $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            return $pdo;
+        }catch (PDOException $e){
+            echo  "Connection failed: " . $e->getMessage();
         }
     }
 }
