170fa45c615ef5fb180088d11912aa9dc5e92b01
JobScholt3n <60648149+JobScholt3n@users.noreply.github.com>
2021-05-20 13:23:03 +0200

    Update database.class.php

--- database.class.php
+++ database.class.php
@@ -9,9 +9,14 @@ class Database
 
     public function connect()
     {
+        try{
+            
         $dsn = "mysql:host=" . $this->servername . ";dbname=" . $this->dbname . ";charset=" . $this->charset;
         $pdo = new PDO($dsn, $this->username, $this->password);
         $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         return $pdo;
+        }catch(PDOexception $e){
+        echo "Connection failed: " . $e->getMessage();
+        }
     }
 }
