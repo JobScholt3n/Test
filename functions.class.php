c4f0699b919243045753510ae487ca594e66b1b0
JobScholt3n <60648149+JobScholt3n@users.noreply.github.com>
2021-05-26 13:57:09 +0200

    Update functions.class.php

--- functions.class.php
+++ functions.class.php
@@ -1,6 +1,7 @@
 <?php
 require_once('database.class.php');
 session_start();
+ob_start();
 class Functions extends Database
 {
 
@@ -106,7 +107,7 @@ class Functions extends Database
         $row   = $sql->fetch(PDO::FETCH_ASSOC);
         if ($count == 1 && !empty($row)) {
             $_SESSION['loggedInUser'] = $row['id'];
-            header("Location: insert.php");
+            header("Location: https://jscholten.gc-webhosting.nl/php_oop_example/");
             exit();
         } else {
             echo "Invalide gebruikersnaam/wachtwoord combinatie";
